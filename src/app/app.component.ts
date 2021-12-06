a9e95c72585746345a454c296533d54dc1cd374e
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2021-12-06 23:19:16 +0800

    working responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, Renderer2,HostListener } from '@angular/core';
 import { initializeApp } from '@firebase/app';
+import { Platform } from '@ionic/angular';
+import { ScreenSizeService } from './services/screen-size/screen-size.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,25 @@ import { initializeApp } from '@firebase/app';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(    private renderer: Renderer2) {
-
+  constructor(
+    private renderer: Renderer2,
+    private platform: Platform,
+    private screensizeService: ScreenSizeService) {
+    this.initApp();
   }
 
+
+
+
+  initApp(){
+    this.platform.ready().then(() => {
+
+      this.screensizeService.onResize(this.platform.width());
+
+    });
+  }
+  @HostListener('window:resize', ['$event'])
+  private onResize(event) {
+    this.screensizeService.onResize(event.target.innerWidth);
+  }
 }
