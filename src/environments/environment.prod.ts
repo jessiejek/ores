7cf6ef04416b7913fe0b3a4e51a57dd75430645f
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2021-12-08 11:27:49 +0800

    working firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+    /*
+    apiKey: "AIzaSyBxioq9hPOIF6BV16fa08LRJdpSXDCG-Ck",
+    authDomain: "oresph-f4a45.firebaseapp.com",
+    databaseURL: "https://oresph-f4a45-default-rtdb.firebaseio.com",
+    projectId: "oresph-f4a45",
+    storageBucket: "oresph-f4a45.appspot.com",
+    messagingSenderId: "1099329131081",
+    appId: "1:1099329131081:web:ab4d87e96ed0487edaa598",
+    measurementId: "G-K4VK3KP4SS"
+    */
+    apiKey: "AIzaSyAIPG7e4pQnyxXGNGyj2VYceteBSXTh5DY",
+    authDomain: "ores-web-app.firebaseapp.com",
+    databaseURL: "https://ores-web-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ores-web-app",
+    storageBucket: "ores-web-app.appspot.com",
+    messagingSenderId: "109552986072",
+    appId: "1:109552986072:web:1c652e78e2ad666868b4ad",
+    measurementId: "G-RQ5JSJEY8S"
+
+  }
 };
